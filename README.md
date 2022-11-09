3acd5893e2b4057b97126e38e9d3dd9b489e6f90
Omega8800 <rohin.mahesh@yahoo.com>
2022-11-09 09:58:08 -0500

    Initial Commit

---
+++ README.md
@@ -0,0 +1,89 @@
+## Overview
+
+This project consists of a serverless data science architecture for ingesting real-time streaming data of geocoordinates to find the optimal path that minimizes the distance covered. 
+
+The following AWS services are leveraged:
+
+- ECR: container registry for Docker image used in Lambda 
+- CloudWatch: collects real-time logs from Lambda function
+- Kinesis: real-time streaming data service for both requests and predictions
+- Lambda: serverless function that performs optimization
+
+## Deployment
+
+This project leverages AWS CLI for deployment of this service. The solution is packaged into a virtual environment, ontainerized using Docker, and registered in ECR. The following components are required to deploy this service:
+
+- Execution role for access to AWS resources. Specifically, for reading items from Kinesis and writing CloudWatch logs
+- Policy in IAM for GetRecord, GetRecords, PutRecord and PutRecords 
+- Kinesis streams for both the request and service stream
+- Lambda function with trigger as the Kinesis request stream and adding key/values for required environment variables
+
+Using the Dockerfile located in the Route_Optimization folder, build the Docker image:
+
+```bash
+docker build -t route_optim:v1 .
+```
+
+To create and run a Docker container:
+
+```bash
+docker run -it --rm \
+    -p 8080:8080 \
+    -e PREDICTIONS_STREAM_NAME="route_optim_pred" \
+    -e AWS_SECRET_KEY_ID=”YOUR_KEY” \
+    -e AWS_SECRET_ACCESS_KEY=”YOUR_SECRET”\
+    -e TEST_RUN="True" \
+    -e AWS_DEFAULT_REGION=”REGION” \
+    route_optim:v1
+
+```
+
+Once the Docker image is built, create an ECR repository: 
+
+```bash
+aws ecr create-repository –repository-name duration-model
+```
+
+Next, push this Docker image to the repositoryUri provided when creating the ECR repository. Finally, create a Lambda function via Container image using the REMOTE_IMAGE:
+
+```bash
+REMOTE_URI= “” 
+REMOTE_TAG=”v1”
+REMOTE_IMAGE=${REMOTE_URI}:${REMOTE_TAG}
+
+LOCAL_IMAGE=”route_optim:v1”
+docker tag ${LOCAL_IMAGE} ${REMOTE_IMAGE}
+docker push ${REMOTE_IMAGE}
+```
+
+## Example Input
+
+```bash
+aws kinesis put-record \
+    --stream-name ${KINESIS_STREAM_INPUT} \
+    --partition-key 1 \
+    --data '{
+        "locations": ["Chicago", "Atlanta", "Los Angeles", "New York City"], 
+        "coordinates": [[41.8781, -87.6298],[33.753746, -84.386330],[34.052235, -118.243683],[40.730610, -73.935242]]
+    }'
+```
+## Reading from Kinesis Stream
+
+```bash
+SHARD_ITERATOR=$(aws kinesis \
+    get-shard-iterator \
+        --shard-id ${SHARD} \
+        --shard-iterator-type TRIM_HORIZON \
+        --stream-name ${KINESIS_STREAM_OUTPUT} \
+        --query 'ShardIterator' \
+)
+
+RECORD=$(aws kinesis get-records --shard-iterator $SHARD_ITERATOR)
+
+echo ${RECORD} | jq -r '.Records[0].Data' | base64 --decode
+```
+
+
+
+
+
---
+++ Route_Optimization/lambda_function.py
@@ -0,0 +1,97 @@
+import base64
+import boto3
+from geopy import distance
+import json
+import numpy as np
+import os
+import uuid
+
+kinesis_client = boto3.client("kinesis")
+
+PRED_STREM = os.getenv("PRED_STREM", "route_optim_pred")
+
+
+class RouteOptimization:
+    """Finds optimal path for a round-trip"""
+
+    def __init__(self, locations, coordinates):
+        """Input for optimization
+
+        Args:
+            locations (List):
+                location names
+            coordinates (List):
+                location geocoordinates
+        """
+        # Initialize required variables
+        self.locations = locations
+        self.coordinates = coordinates
+        self.indexes = list(range(0, len(self.locations)))
+        self.latitude = [x[0] for x in self.coordinates]
+        self.longitude = [x[1] for x in self.coordinates]
+        self.dict_indexes = dict(zip(self.indexes, self.locations))
+        self.dict_locations = dict(zip(self.locations, self.coordinates))
+        self.ids = self.indexes[1:]
+        self.coordinates_array = np.array([self.latitude, self.longitude]).T[1:]
+        self.route = [0]
+
+    def get_route(self):
+        """Performs optimization
+
+        Returns:
+            route_dict (Dict):
+                dictionary containing of optimal route and coordinates
+        """
+        # Perform optimization
+        for x in range(len(self.latitude) - 1):
+            previous_lat = self.latitude[self.route[-1]]
+            previous_lon = self.longitude[self.route[-1]]
+
+            distances = [
+                distance.distance((x[0], x[1]), (previous_lat, previous_lon))
+                for x in self.coordinates_array
+            ]
+
+            closest_location = np.array(distances).argmin()
+            self.route.append(self.ids[closest_location])
+            self.ids = np.delete(self.ids, closest_location, axis=0)
+            self.coordinates_array = np.delete(
+                self.coordinates_array, closest_location, axis=0
+            )
+
+        # Prepare and return optimal route and coordinates
+        self.route.append(0)
+        route_locations = [self.dict_indexes[x] for x in self.route]
+        route_coordinates = [self.dict_locations[x] for x in self.dict_locations]
+        route_dict = {k: v for k in route_locations for v in route_coordinates}
+        return route_dict
+
+
+def lambda_handler(event, context):
+    predictions_events = []
+
+    for record in event["Records"]:
+        encoded_data = record["kinesis"]["data"]
+        decoded_data = base64.b64decode(encoded_data).decode("utf-8")
+        viapoints = json.loads(decoded_data)
+
+        # Get the optimal route
+        prediction_id = uuid.uuid4()  # Randomly generated UUID
+        locations = viapoints["locations"]
+        coordinates = viapoints["coordinates"]
+        optimal_route = RouteOptimization(locations, coordinates).get_route()
+        prediction_event = {
+            "UUID": str(prediction_id),
+            "route": optimal_route,
+        }
+
+        # Put prediction event in Kinesis Stream
+        kinesis_client.put_record(
+            StreamName=PRED_STREM,
+            Data=json.dumps(prediction_event),
+            PartitionKey=str(prediction_id),
+        )
+
+        predictions_events.append(prediction_event)
+
+    return {"predictions": predictions_events}
